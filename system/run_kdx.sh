727ed224537a3e53e1e9cbd28c2ecc01ea7fed66
datolg123 <datolg123@gmail.com>
2024-12-31 18:16:34 +0700

    fixed wandb errors

--- system/run_kdx.sh
+++ system/run_kdx.sh
@@ -16,12 +16,12 @@
 
 # pip install audiomentations
 # pip install calmsize
- 
+
 # test on SLEEP dataset
-python main.py -data SLEEP -m harcnnbn -algo FedKDX -gr 500 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 4 -ld True -dev cuda -lr 0.01 -mlr 0.01 -Ts 0.9
+python main.py -data SLEEP -m harcnnbn -algo FedKDX -gr 500 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 4 -ld True -dev cuda -lr 0.01 -mlr 0.01 -Ts 0.9 -wb true
 
 
-python main.py -data SLEEP -m harcnn -algo FedKD -gr 500 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 4 -ld True -dev cuda  -lr 0.01 -mlr 0.01 -Ts 0.9
+python main.py -data SLEEP -m harcnn -algo FedKD -gr 500 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 4 -ld True -dev cuda  -lr 0.01 -mlr 0.01 -Ts 0.9 -wb true
 
 python main.py -data SLEEP -m harcnnbn -algo FedKDX -gr 500 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 4 -ld True -dev cuda  -lr 0.01 -mlr 0.01 -Ts 0.9
 
